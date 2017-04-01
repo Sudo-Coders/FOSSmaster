b8cd590ab3ea782c54ccd0796090941354af8efe
= <=>
2017-04-01 16:01:20 +0530

    Splash Screen added

---
+++ app/src/main/java/com/hackathon/sudocoders/fossmaster/Splash.java
@@ -0,0 +1,30 @@
+package com.hackathon.sudocoders.fossmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+public class Splash extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(Splash.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
